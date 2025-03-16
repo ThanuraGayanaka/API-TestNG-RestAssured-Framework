95cb76c15b310f70136eeefd2310bcb326608047
Thanura gayanaka <57209475+ThanuraGayanaka@users.noreply.github.com>
2025-03-17 04:50:30 +0530

    Added RequestSpecification

--- src/test/java/ZipCodeTest.java
+++ src/test/java/ZipCodeTest.java
@@ -1,9 +1,14 @@
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class ZipCodeTest {
+
+    private static  RequestSpecification requestSpec;
 
     @Test(dataProvider = "zipcodes", dataProviderClass = Utility.ZipCodeDataProvider.class)
     public void testZipCode(String country, String zipCode, String expectedCity) {
@@ -17,5 +22,27 @@ public class ZipCodeTest {
                 .then()
                 .statusCode(200)
                 .body("places[0].'place name'", equalTo(expectedCity));
+    }
+
+
+
+@BeforeClass
+    public void CreaterequestSpec(){
+
+        requestSpec = new RequestSpecBuilder().
+                setBaseUri("https://api.zippopotam.us").build();
+
+
+    }
+
+@Test
+    public void usedRequestSpec(){
+
+
+        given().
+                spec(requestSpec).
+                when().get("us/90210").
+                then().assertThat().statusCode(200);
+
     }
 }
